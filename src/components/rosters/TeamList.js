016d6598f0cbc7f0c82057c9e6ab019bd7097395
Robert East <RobbyXPS@gmail.com>
2019-10-03 11:19:57 -0700

    fixed mobile spacing that broke with last commit, fixed dropdown menu so it is scrollable with more data in db

--- src/components/rosters/TeamList.js
+++ src/components/rosters/TeamList.js
@@ -69,7 +69,24 @@ class TeamList extends React.Component {
               </DropdownToggle>
     */}
 
-              <DropdownMenu>
+              <DropdownMenu
+                modifiers={{
+                  setMaxHeight: {
+                    enabled: true,
+                    order: 890,
+                    fn: data => {
+                      return {
+                        ...data,
+                        styles: {
+                          ...data.styles,
+                          overflow: "auto",
+                          maxHeight: 300
+                        }
+                      };
+                    }
+                  }
+                }}
+              >
                 {teams &&
                   teams.map(team => {
                     return (
